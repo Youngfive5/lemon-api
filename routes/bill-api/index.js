18cbcf1f271a0f55d7d1a26ba783b1cd29022ff2
Lucky <963190000@qq.com>
2019-01-12 10:42:52 +0800

    lcx-api提交新增账单功能

--- routes/bill-api/index.js
+++ routes/bill-api/index.js
@@ -5,18 +5,18 @@ var mymongo = require('mymongo1610');
 // 查询账单接口 -- 待补充
 function getBill(req, res, next) {
     // 应该传入时间 通过时间返回相对应的账单 
-    mymongo.find('bill_list', (err) => {
+    mymongo.find('bill_list', (err, result) => {
         if (err) {
             res.json({ code: 0, msg: err });
         } else {
-            res.json({ code: 1, ms: '删除成功!' });
+            res.json({ code: 1, msg: '成功!', data: result });
         }
     });
 };
 
 // 新增账单接口
 function addBill(req, res, next) {
-    // res.json({ 'cont': '/api/addBill接口返回值' });
+    console.log(req.body)
     var uid = req.body.uid; //区分用户
     var type = req.body.type; //收入-> 0，支出-> 1 
     var money = req.body.money;//金额
@@ -24,7 +24,7 @@ function addBill(req, res, next) {
     var icon = req.body.icon; //图标类名
     // 账单创建时间 
     // tolocaleString(把时间变成年月日时分).split(切割空格取年月日)
-    var timer = new Date().toLocaleString().split(' ')[1];
+    var timer = req.body.timer;
     // 后端非空判断
     if (!uid || !icon || !type || !money || !timer || !intro) {
         return res.json({ code: 3, msg: '丢失参数' })
--- routes/user-api/index.js
+++ routes/user-api/index.js
@@ -13,40 +13,8 @@ function addUser(req, res, next) {
         })
     }
 };
-// 5c3594243fe6f828203607cd
-// 5c35943e4be8f110e44121c8
 
 
 module.exports = {
     addUser: addUser,
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// user 下的index.js 写的是users.js下所有接口的回调函数
-// 所以模块抛出一个对象 返回所有接口的ck回调
-
-// var addUser = function (req, res, next) {
-
-// }
-// var delUser = function (req, res, next) {
-
-// }
-
-// 抛出对象
-
-// module.exports = {
-//     addUser: addUser,
-//     delUser: delUser
-// }
